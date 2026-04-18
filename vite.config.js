179f57aa08e5e1f8f0e113c70f13621036da4c6f
codelusitan <codelusitan@gmail.com>
2026-04-18 14:49:59 +0100

    fixs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // relative paths – funciona em GitHub Pages com qualquer repo name
+  base: '/Portofolio'
 })
